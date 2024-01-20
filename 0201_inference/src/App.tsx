191de2ac1d1ff82139cc97433dbe7ce1889eb020
Eduardo <eduardo.benigno15@gmail.com>
2024-01-20 11:45:46 -0300

    fix: 0201 class

--- 0201_inference/src/App.tsx
+++ 0201_inference/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './Button';
 
 function App() {
 
+  const [total, setTotal] = useState(0)
+
+  const increment = () => {
+    setTotal((total) => total + 1)
+  }
+
   return (
     <>
-      <p>tsx content</p>
+      <p>{total}</p>
+      <button onClick={increment}>Increment</button>
+      <Button/> {/* to pass props, will be necessary to type them */}
     </>
   )
 }
