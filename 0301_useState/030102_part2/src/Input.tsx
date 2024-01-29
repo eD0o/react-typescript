78b12ab4ceda81285c1c5afa97c081e321b903d7
Eduardo <eduardo.benigno15@gmail.com>
2024-01-29 16:51:06 -0300

    feat: finishing class 0301 and its exercise

--- 0301_useState/030102_part2/src/Input.tsx
+++ 0301_useState/030102_part2/src/Input.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 type InputProps = React.ComponentProps<"input"> & {
   label: string
   marginBottom?: string
+  setState: React.Dispatch<React.SetStateAction<string>>
 }
 
-const Input = ({ label, marginBottom, ...props }: InputProps) => {
+const Input = ({ label, marginBottom, setState, ...props }: InputProps) => {
   return (
     <div style={{ marginBottom }}>
       <label htmlFor={label}>{label}</label>
-      <input id={label} name={label} type="text" {...props} />
+      <input id={label} name={label} type="text" onChange={({ currentTarget }) => setState(currentTarget.value)} {...props} />
     </div>
   )
 }
