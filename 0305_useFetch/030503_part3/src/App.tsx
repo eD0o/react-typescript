167394f269881ffc613997315ca533e61a60143f
Eduardo <eduardo.benigno15@gmail.com>
2024-02-01 05:31:22 -0300

    feat: finishing class 0305

--- 0305_useFetch/030503_part3/src/App.tsx
+++ 0305_useFetch/030503_part3/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetch from './useFetch';
 
 type Product = {
@@ -11,12 +11,35 @@ type Product = {
 }
 
 function App() {
-  
+
+  const [id, setId] = useState("p001");
+
   const products = useFetch<Product[]>("https://data.origamid.dev/produtos/")
+
+  const product = useFetch<Product>(`https://data.origamid.dev/produtos/${id}`, {
+    cache: "force-cache"
+  })
 
   return (
     <>
-      <p>tsx content</p>
+      <section className="flex">
+        <div>
+          {products.data
+            && products.data.map((product) => (
+              <button onClick={() => setId(product.id)} style={{ fontSize: "1rem" }} key={product.id}>{product.id}: {product.nome} </button>
+            ))}
+        </div>
+        <div>
+          {product.data && (
+            <ul>
+              <li>ID: {product.data.id}</li>
+              <li>Name: {product.data.nome}</li>
+              <li>Description: {product.data.descricao}</li>
+              <li>Quantity: {product.data.quantidade}</li>
+            </ul>
+          )}
+        </div>
+      </section>
     </>
   )
 }
