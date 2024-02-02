91c89cec2214e6bb6ffb6e8d0dbb1d2b968ec2ff
Eduardo <eduardo.benigno15@gmail.com>
2024-02-02 11:05:16 -0300

    feat: preparing class 0306 part 2

---
+++ 0306_useContext/030602_part2/src/Content.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useUi } from './UiContext'
+
+const Content = () => {
+  const { dark } = useUi();
+  return (
+    <>
+      <h1>
+        {dark ? 'dark' : 'light'}
+      </h1>
+    </>
+  )
+}
+
+export default Content
