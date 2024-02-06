451d7d0cbe45f9fbdb04ccc520548106859755b4
Eduardo <eduardo.benigno15@gmail.com>
2024-02-06 19:05:07 -0300

    feat: preparing class 0408

---
+++ 0408_monthButton/src/Components/DateRange.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import DateInput from './DateInput'
+import { useData } from '../Context/DataContext'
+
+const DateRange = () => {
+
+  const { start, setStart, end, setEnd } = useData();
+
+  return (
+    <form onSubmit={(e) => e.preventDefault} className="box flex">
+      <DateInput label="Start" value={start} onChange={({ target }) => setStart(target.value)} />{start}
+      <DateInput label="End" value={end} onChange={({ target }) => setEnd(target.value)} />{end}
+    </form>
+  )
+}
+
+export default DateRange
