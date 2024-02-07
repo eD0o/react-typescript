20f7c6f7c8f076b5fa7c5475e6338fb3eb220f9e
Eduardo <eduardo.benigno15@gmail.com>
2024-02-07 16:50:47 -0300

    feat: class 0409

--- 0409_salesSummary/src/Pages/Summary.tsx
+++ 0409_salesSummary/src/Pages/Summary.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
 import { useData } from '../Context/DataContext';
 
 const Summary = () => {
 
   const { data } = useData();
 
-  console.log(data);
-  
+
+
+  if (data === null) return null
+
   return (
-    <div>Summary</div>
+    <section>
+      <div className="summary flex mb">
+        <div className='box'>
+          <h2>Sales</h2>
+          <p>
+            {data.filter((i) => i.status !== 'falha').reduce((acc, item) => acc + item.preco, 0).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </p>
+        </div>
+        <div className='box'>
+          <h2>Received</h2>
+          <p>
+            {data.filter((i) => i.status === 'pago').reduce((acc, item) => acc + item.preco, 0).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </p>
+        </div>
+        <div className='box'>
+          <h2>Processing</h2>
+          <p>
+            {data.filter((i) => i.status === 'processando').reduce((acc, item) => acc + item.preco, 0).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </p>
+        </div>
+      </div>
+      <div className="box">Graphics</div>
+    </section>
   )
 }
 
