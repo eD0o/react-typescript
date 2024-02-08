060fcbe782d675a948f703df5fe9c8ebf0818fd3
Eduardo <eduardo.benigno15@gmail.com>
2024-02-08 20:13:00 -0300

    feat: class 0411

--- 0411_sidenav/src/Components/Header.tsx
+++ 0411_sidenav/src/Components/Header.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DateRange from './DateRange';
 import Months from './Months';
 
 const Header = () => {
+  
+  const [title, setTitle] = useState('Summary')
 
   return (
     <>
       <header className='mb'>
-        <div className='mb'>
+        <div className='daterange mb'>
           <DateRange />
+          <h1 className='box bg-3'>{title}</h1>
         </div>
         <Months />
       </header>
--- 0411_sidenav/src/Components/Sidenav.tsx
+++ 0411_sidenav/src/Components/Sidenav.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import fintech from '../assets/fintech.svg';
+import summary from '../assets/icons/resumo.svg';
+import sales from '../assets/icons/vendas.svg';
+import webhooks from '../assets/icons/webhooks.svg';
+import settings from '../assets/icons/configuracoes.svg';
+import contact from '../assets/icons/contato.svg';
+import logout from '../assets/icons/sair.svg';
+import FintechSVG from '../assets/FintechSVG';
 
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <nav className='sidenav box bg-3'>
+      <FintechSVG title='Fintech Logo'/>
+      <ul>
+        <li>
+          <span><img src={summary} alt="" /></span>
+          <a href="">Summary</a>
+        </li>
+        <li>
+          <span><img src={sales} alt="" /></span>
+          <a href="">Sales</a>
+        </li>
+        <li>
+          <span><img src={webhooks} alt="" /></span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span><img src={settings} alt="" /></span>
+          <a>Settings</a>
+        </li>
+        <li>
+          <span><img src={contact} alt="" /></span>
+          <a>Contact</a>
+        </li>
+        <li>
+          <span><img src={logout} alt="" /></span>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
