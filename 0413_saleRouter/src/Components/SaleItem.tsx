775ab39571c75514165fcf2f93e7b0adf84e36a7
Eduardo <eduardo.benigno15@gmail.com>
2024-02-08 20:45:45 -0300

    feat: class 0413

--- 0413_saleRouter/src/Components/SaleItem.tsx
+++ 0413_saleRouter/src/Components/SaleItem.tsx
@@ -1,9 +1,10 @@
+import { NavLink } from 'react-router-dom'
 import { ISale } from '../Context/DataContext'
 
 const SaleItem = ({ sale }: { sale: ISale }) => {
   return (
     <div className='sale box'>
-      <a href="" style={{ fontFamily: "monospace" }}>{sale.id}</a>
+      <NavLink to={`/sales/${sale.id}`} style={{ fontFamily: "monospace" }}>{sale.id}</NavLink>
       <div>{sale.nome}</div>
       <div>{sale.preco.toLocaleString('pt-br', {style:'currency', currency: 'BRL'})}</div>
     </div>
